50f96bff1109347e5e11770e3f355574a61a6028
Eron <erony1204@gmail.com>
2022-12-14 21:02:15 -0800

    Update to Player Movement
    Movement now runs left to right, animated and flipping in direction.

--- PlayerMovement.cs
+++ PlayerMovement.cs
@@ -10,6 +10,8 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody2D rb2D;
     private Animator myAnimator;
 
+    private bool facingRight = true;
+
     //variables to play with
     public float speed = 2.0f;
     public float horizMovement; //=1 or -1 or 0
@@ -32,5 +34,20 @@ public class PlayerMovement : MonoBehaviour
     {
         //moves the player left and right
         rb2D.velocity = new Vector2(horizMovement*speed, rb2D.velocity.y);
+        Flip(horizMovement);
+        myAnimator.SetFloat("speed", Mathf.Abs(horizMovement));
+    }
+
+    //flipping fuction
+    private void Flip (float horizontal)
+    {
+        if (horizontal < 0 && facingRight || horizontal > 0 && !facingRight)
+        {
+            facingRight = !facingRight;
+
+            Vector3 theScale = transform.localScale;
+            theScale.x *= -1;
+            transform.localScale = theScale;
+        }
     }
 }
